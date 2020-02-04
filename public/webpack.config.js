e291155b7b3ad8dbb18eb5b7a49ca00de696d552
Pedja <vpedja12@gmail.com>
2020-02-03 23:48:48 -0800

    fixes

--- public/webpack.config.js
+++ public/webpack.config.js
@@ -1,6 +1,4 @@
-const SWPrecacheWebpackPlugin = require("sw-precache-webpack-plugin");
-const WebpackPwaManifest = require("webpack-pwa-manifest");
-const path = require("path");
+const BundleAnalyzerPlugin = require("webpack-bundle-analyzer").BundleAnalyzerPlugin;
 
 const config = {
   entry: {
@@ -12,6 +10,7 @@ const config = {
     filename: "[name].bundle.js"
   },
   mode: "development",
+  plugins: [new BundleAnalyzerPlugin()],
   module: {
     rules: [
       {
@@ -26,29 +25,5 @@ const config = {
       }
     ]
   },
-  plugins: [
-    new SWPrecacheWebpackPlugin({
-      cacheId: "my-domain-cache-id",
-      dontCacheBustUrlsMatching: /\.\w{8}\./,
-      filename: "service-worker.js",
-      minify: true,
-      staticFileGlobsIgnorePatterns: [/\.map$/, /manifest\.json$/]
-    }),
-    new WebpackPwaManifest({
-      name: "Budget Tracker",
-      short_name: "Budget Tracker",
-      description: "An application that allows you to keep track of all your budgets.",
-      background_color: "#01579b",
-      theme_color: "#ffffff",
-      "theme-color": "#ffffff",
-      start_url: "/",
-      icons: [{
-        src: path.resolve("images/icons/icon-192x192.png"),
-        sizes: [96, 128, 192, 256, 384, 512],
-        destination: path.join("images", "icons")
-      }]
-    })
-  ]
-};
-
+}
 module.exports = config;
