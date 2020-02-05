c41a5b09d0acd451fd82fb6d459bf929d2ad3e2f
Pedja <vpedja12@gmail.com>
2020-02-04 16:39:30 -0800

    Built app

--- public/webpack.config.js
+++ public/webpack.config.js
@@ -1,4 +1,6 @@
-const BundleAnalyzerPlugin = require("webpack-bundle-analyzer").BundleAnalyzerPlugin;
+const SWPrecacheWebpackPlugin = require("sw-precache-webpack-plugin");
+const WebpackPwaManifest = require("webpack-pwa-manifest");
+const path = require("path");
 
 const config = {
   entry: {
@@ -10,7 +12,6 @@ const config = {
     filename: "[name].bundle.js"
   },
   mode: "development",
-  plugins: [new BundleAnalyzerPlugin()],
   module: {
     rules: [
       {
@@ -25,5 +26,29 @@ const config = {
       }
     ]
   },
-}
+  plugins: [
+    new SWPrecacheWebpackPlugin({
+      cacheId: "my-domain-cache-id",
+      dontCacheBustUrlsMatching: /\.\w{8}\./,
+      filename: "service-worker.js",
+      minify: true,
+      staticFileGlobsIgnorePatterns: [/\.map$/, /manifest\.json$/]
+    }),
+    new WebpackPwaManifest({
+      name: "Newsy app",
+      short_name: "Newsy",
+      description: "An application that allows you to view different news articles and save your favorites.",
+      background_color: "#01579b",
+      theme_color: "#ffffff",
+      "theme-color": "#ffffff",
+      start_url: "/",
+      icons: [{
+        src: path.resolve("assets/images/icons/android-chrome-192x192.png"),
+        sizes: [96, 128, 192, 256, 384, 512],
+        destination: path.join("assets", "icons")
+      }]
+    })
+  ]
+};
+
 module.exports = config;
